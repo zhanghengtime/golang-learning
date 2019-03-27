afe99bc75448a0c2a0ebc5d97891f1123e0a86cb
zhanghengtime <zhanghengti@126.com>
2019-03-27 21:25:56 +0800

    new

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -1,9 +1,19 @@
 package main
 
-import (
+import(
 	"fmt"
 )
 
-func main()  {
-	fmt.Println("hello World")
+var s = "Hello World"
+
+func main(){
+	fmt.Printf("print 's' variable from outer block %v\n", s)
+	b:=true
+	if b{
+		fmt.Printf("printing 'b' varible from outer block %v\n",b)
+		i:=42
+		if b!=false{
+			fmt.Printf("printing 'i' variable from outer block %v\n",i)
+		}
+	}
 }
