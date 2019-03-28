5f5bf1a6731d168174779e6076a0af1724429fdb
zhanghengtime <zhanghengti@126.com>
2019-03-28 11:38:05 +0800

    3.28

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -1,19 +1,17 @@
 package main
 
-import(
-	"fmt"
-)
+import "fmt"
 
-var s = "Hello World"
-
-func main(){
-	fmt.Printf("print 's' variable from outer block %v\n", s)
-	b:=true
-	if b{
-		fmt.Printf("printing 'b' varible from outer block %v\n",b)
-		i:=42
-		if b!=false{
-			fmt.Printf("printing 'i' variable from outer block %v\n",i)
-		}
+func feedMe(portion int, eaten int) int {
+	eaten = portion + eaten
+	if eaten >= 5 {
+		fmt.Printf("I'm full! I've eaten %d\n", eaten)
+		return feedMe(1, 0)
 	}
+	fmt.Printf("I'm still hungry! I've eaten %d\n", eaten)
+	return feedMe(portion, eaten)
+}
+
+func main() {
+	fmt.Println(feedMe(1, 0))
 }
