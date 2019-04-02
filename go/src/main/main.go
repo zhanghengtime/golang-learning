9af64c2902502203a4b91b6cd742e151a9d34266
zhanghengtime <zhanghengti@126.com>
2019-04-02 21:36:01 +0800

    4.2

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -2,32 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func sender(c chan string) {
-	t := time.NewTicker(1 * time.Second)
-	for {
-		c <- "I'm sending a message"
-		<-t.C
-	}
-}
-
 func main() {
-	message := make(chan string)
-	stop := make(chan bool)
-	go sender(message)
-	go func() {
-		time.Sleep(time.Second * 2)
-		fmt.Println("Time's up!")
-		stop <- true
-	}()
-	for {
-		select {
-		case <-stop: //通道关闭
-			return
-		case msg := <-message:
-			fmt.Println(msg)
+	var c1, c2, c3 chan int
+	var i1, i2 int
+	select {
+	case i1 = <-c1:
+		fmt.Printf("received ", i1, " from c1\n")
+	case c2 <- i2:
+		fmt.Printf("sent ", i2, " to c2\n")
+	case i3, ok := (<-c3):
+		if ok {
+			fmt.Printf("received ", i3, " from c3\n")
+		} else {
+			fmt.Printf("c3 is closed\n")
 		}
+	default:
+		fmt.Printf("no communication\n")
 	}
 }
